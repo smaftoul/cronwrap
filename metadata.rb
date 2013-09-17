8843d7ff2946da57447d787a3121b0da1fb5813d
smaftoul <samuel.maftoul@gmail.com>
2013-09-17 11:46:19 +0200

    Don't require a README.md, Have a cookbook's long description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "SensioLabs"
 maintainer_email "samuel.maftoul@sensiolabs.com"
 license          "All rights reserved"
 description      "Installs/Configures cronwrap"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description "Installs/Configures cronwrap using the python cookbook, also configures a mail client"
 version          "0.1.0"
 
 depends "python"
